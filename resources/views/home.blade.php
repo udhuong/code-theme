d015f1ad7a59def4103137003bb54577f670679c
udhuong <huong.ung@pirago.work>
2022-07-12 09:18:29 +0700

    clean code

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,4 @@
 @extends('master')
 @section('content')
-    <div class="md:hidden">
-        <div class="h-[calc(100vh-45px-100px)] bg-red-500">
-            <img src="{{ asset('images/banner.png') }}" class="object-cover w-full h-full mx-auto" />
-        </div>
-    </div>
-    <div class="hidden md:block">
-        @include('home_desktop')
-    </div>
+
 @stop
--- routes/web.php
+++ routes/web.php
@@ -16,36 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 })->name('home');
-Route::get('home-desktop', function () {
-    return view('home_desktop');
-});
-Route::get('guide', function () {
-    return view('guide');
-});
-Route::get('spot', function () {
-    return view('spot');
-});
-Route::get('stamps', function () {
-    return view('stamp-list');
-});
-Route::get('stamp-get/{char}', function ($char) {
-    return view('stamp-get')->with(['char' => $char]);
-});
-Route::get('reward', function () {
-    return view('reward');
-});
-Route::get('reward-exchanged', function () {
-    return view('reward-exchanged');
-});
-Route::get('prize', function () {
-    return view('prize');
-});
-Route::get('prize-inactive', function () {
-    return view('prize-inactive');
-});
-Route::get('award', function () {
-    return view('award');
-});
-Route::get('form', function () {
-    return view('form');
-});
